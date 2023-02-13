1e391ea363f23df97c899759e35b6d67fb44dd69
Louise <louise.n.bolden@gmail.com>
2023-02-13 10:49:26 +0000

    Introduce factory method for tests and use this factory method to initialise easier-to-read fixtures.

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,13 +1,18 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
+# A very basic factory method - would use e.g. FactoryBot
+def build_item(name:, sell_in:, quality:)
+  Item.new(name, sell_in, quality)
+end
+
 describe GildedRose do
+  let(:item) { build_item(name: "Test Item", sell_in: 1, quality: 1) }
+  let(:items) { [item] }
 
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "Test Item"
     end
   end
-
 end
