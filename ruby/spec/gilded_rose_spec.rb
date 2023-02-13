256b110519ddec757121304513da0ad9307b0246
Louise <louise.n.bolden@gmail.com>
2023-02-13 11:44:45 +0000

    Create spec subdirectory for tests

---
+++ ruby/spec/gilded_rose_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../gilded_rose'
+
+# A very basic factory method - would use e.g. FactoryBot
+def build_item(name:, sell_in:, quality:)
+  Item.new(name, sell_in, quality)
+end
+
+describe GildedRose do
+  let(:item) { build_item(name: "Default Item", sell_in: 1, quality: 1) }
+  let(:items) { [item] }
+
+  subject { GildedRose.new(items) }
+
+  describe "#update_quality" do
+    before { subject.update_quality }
+
+    it "does not change the name" do
+      expect(items[0].name).to eq "Default Item"
+    end
+
+    context "when an item's quality decreases over time" do
+      context "when an item's quality value is zero" do
+        let(:item) { build_item(name: "Default Item", sell_in: 1, quality: 0) }
+
+        it "does not reduce the quality value for that item" do
+          expect(items[0].quality).to eq(0)
+        end
+      end
+    end
+  end
+end
